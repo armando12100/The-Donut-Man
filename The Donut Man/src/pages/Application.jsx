6d1809694fb5335ca81fb318a7248283b4190326
Armando Sanchez <asan5900@gmail.com>
2024-05-01 18:07:19 -0700

    Application Page Started

--- The Donut Man/src/pages/Application.jsx
+++ The Donut Man/src/pages/Application.jsx
@@ -1,9 +1,89 @@
 const Application = () => {
   return (
     <>
-      <form action="" className="flex justify-center">
-        <h1 className="text-3xl font-bold">Application</h1>
-      </form>
+      <div className="flex justify-center pb-12 mt-8">
+        <form
+          action=""
+          className="flex-col items-center text-center justify-center rounded-2xl
+          bg-gradient-to-br from-yellow-300 to-pink-300 w-10/12 flex-wrap "
+        >
+          <div>
+            <h1 className="text-5xl font-bold text-white pt-3">Apply Today!</h1>
+          </div>
+
+          <div className="flex">
+            <div className="flex-col">
+              <div className="py-4">
+                <label className="label">First Name:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+
+              <div className="py-4">
+                <label className="label">Address:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+
+              <div className="py-4">
+                <label className="label">City:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+
+              <div className="py-4">
+                <label className="label">Phone:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+            </div>
+
+            <div className="flex-col">
+              <div className="py-4">
+                <label className="label">Last Name:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+
+              <div className="py-4">
+                <label className="label">Zip Code:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+
+              <div className="py-4">
+                <label className="label">State:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+
+              <div className="py-4">
+                <label className="label">Email:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+            </div>
+          </div>
+
+          {/* <div className="flex-col">
+              <div className="flex items-center justify-center my-10">
+                <label className="label">Last Name:</label>
+                <input type="text" name="name" className="input" />
+
+                <label className="label">Apartment/Unit #:</label>
+                <input type="text" name="name" className="input" />
+
+                <label className="label">State:</label>
+                <input type="text" name="name" className="input" />
+
+                <label className="label">Zip Code:</label>
+                <input type="text" name="name" className="input" />
+
+                <label className="label">Email:</label>
+                <input type="text" name="name" className="input" />
+              </div>
+            </div> */}
+          <button
+            className="bg-yellow-300 font-bold 
+        rounded-3xl py-4 px-20 text-white shadow-xl hover:border-yellow-400 
+        cursor-pointer hover:bg-pink-300 transition duration-500 mb-10"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </>
   );
 };
